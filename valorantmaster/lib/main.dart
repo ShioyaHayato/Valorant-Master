7e470ea978c44742bb4aa3af7b1a4640d03be3ab
ShioyaHayato <freeshioyahayato@gmail.com>
2024-01-25 18:05:08 +0900

    1/25　職場終わり

--- valorantmaster/lib/main.dart
+++ valorantmaster/lib/main.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(VMlogo());
+void main() => runApp(VmLogo());
 
-class VMlogo extends StatelessWidget {
+class VmLogo extends StatelessWidget {
+  const VmLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-        ),
+        appBar: AppBar(),
         body: Column(
           children: [
             Center(
-              child:
-              Align(
-               alignment: Alignment.topCenter,
-               child: Image.asset('images/VM Icon.png',
-                                    width: 10,
-                                    height: 10,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset('images/vmIcon.png',
+                  width: 100,
+                  height: 100,
+                ),
               ),
-             ),
+            ),
+            SizedBox(height: 100),
+            Center(
+              child: GestureDetector(
+                onTap: () {
+                launch('https://www.google.com');
+                },
+                child: Image.asset('images/Agent.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
             ),
           ],
         ),
