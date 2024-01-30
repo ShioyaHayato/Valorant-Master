aa161b9c7d6b5be570a95aaaab349f6491688818
ShioyaHayato <freeshioyahayato@gmail.com>
2024-01-30 10:03:46 +0900

    1/30

---
+++ valorantmaster/lib/agentfolder/duelist/reyna.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+//agent_pageに書いてある
+
+class ReynaPage extends StatelessWidget {
+  const ReynaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(backgroundColor: Colors.red[900], title: const Text('Reyna')),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/MainPage.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Center(),
+      ),
+    );
+  }
+}
