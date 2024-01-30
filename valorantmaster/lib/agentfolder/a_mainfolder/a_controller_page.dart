8aebcf1848bee3315045ff8ae3e9b01573530948
ShioyaHayato <freeshioyahayato@gmail.com>
2024-01-30 10:34:55 +0900

    1/30

--- valorantmaster/lib/agentfolder/a_mainfolder/a_controller_page.dart
+++ valorantmaster/lib/agentfolder/a_mainfolder/a_controller_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:valorantmaster/agentfolder/controller/astra.dart';
+import 'package:valorantmaster/agentfolder/controller/brimstone.dart';
+import 'package:valorantmaster/agentfolder/controller/harbor.dart';
+import 'package:valorantmaster/agentfolder/controller/omen.dart';
+import 'package:valorantmaster/agentfolder/controller/viper.dart';
 
 //main.dartをインポートすることで、agent_pageからでもmainの情報を見に行くことが出来る
 
@@ -12,14 +17,64 @@ class ControllerPage extends StatelessWidget {
           backgroundColor: Colors.red[900], //colorコードは#fffffじゃない、ややこし
           title: const Text('Controller') //フォントを変更したい todo:
           ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/MainPage.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/MainPage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
-        child: const Center(),
+          GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            children: [
+              for (int i = 1; i < 9; i++)
+                GestureDetector(
+                  onTap: () {
+                    if (i == 1) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const AstraPage()),
+                      );
+                    }
+                     else if (i == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const BrimstonePage()),
+            );
+          }
+           else if (i == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HarborPage()),
+            );
+          }
+           else if (i == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const OmenPage()),
+            );
+          }
+           else if (i == 5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ViperPage()),
+            );
+          }
+
+          //8人目のデュエリストが出たら追加する
+
+
+                  },
+                  child: Image.asset('images/Controller$i.png'),
+                ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- valorantmaster/lib/agentfolder/a_mainfolder/a_sentinel_page.dart
+++ valorantmaster/lib/agentfolder/a_mainfolder/a_sentinel_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:valorantmaster/agentfolder/sentinel/chamber.dart';
+import 'package:valorantmaster/agentfolder/sentinel/cypher.dart';
+import 'package:valorantmaster/agentfolder/sentinel/deadlock.dart';
+import 'package:valorantmaster/agentfolder/sentinel/killjoy.dart';
+import 'package:valorantmaster/agentfolder/sentinel/sage.dart';
 
 //main.dartをインポートすることで、agent_pageからでもmainの情報を見に行くことが出来る
 
@@ -12,14 +17,64 @@ class SentinelPage extends StatelessWidget {
           backgroundColor: Colors.red[900], //colorコードは#fffffじゃない、ややこし
           title: const Text('Sentinel') //フォントを変更したい todo:
           ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/MainPage.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/MainPage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
-        child: const Center(),
+          GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            children: [
+              for (int i = 1; i < 9; i++)
+                GestureDetector(
+                  onTap: () {
+                    if (i == 1) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const DeadlockPage()),
+                      );
+                    }
+                     else if (i == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CypherPage()),
+            );
+          }
+           else if (i == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const KilljoyPage()),
+            );
+          }
+           else if (i == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SagePage()),
+            );
+          }
+           else if (i == 5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ChamberPage()),
+            );
+          }
+
+          //8人目のデュエリストが出たら追加する
+
+
+                  },
+                  child: Image.asset('images/Sentinel$i.png'),
+                ),
+            ],
+          ),
+        ],
       ),
     );
   }
